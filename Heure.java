dc9edcc3c07a408b0fcd1736eb392ddb02df835f
Mazaud <nicolas.mazaud.sio@gmail.com>
2018-09-11 15:51:56 +0200

    Constructeur Heure(int, int)

--- Heure.java
+++ Heure.java
@@ -1,5 +1,18 @@
 public class Heure{
 		private int heures;
 		private int minutes;
+		
+		public Heure(int, int){
+			if(heures < 0 || heures > 23){
+				this.heures = 0;
+			}else{
+				this.heures = heures;
+			} 
+			if(minutes < 0 || minutes > 59){
+				this.minutes = 0;
+			}else{
+				this.minutes = minutes;
+			}
+		}
 				
 }
